4f97bf3c366ae966d10ebd4d19c9a45098ec1f66
Kaleb Ercanbrack <kaleb.ercanbrack@arivo.com>
2024-07-03 13:56:59 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const title = ref("title");
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-text-field v-model="title"></v-text-field>
+        <v-card :title></v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createVuetify } from "vuetify";
+import 'vuetify/styles'
+
+createApp(App).use(createVuetify()).mount("#app");
